00c223d04ecfe9467be87e29bc19e706980360c5
Simao Gomes Viana <xdevs23@outlook.com>
2016-10-20 14:32:04 +0200

    Revert "Use safe charging limits"
    This reverts commit 5879747ae049976d94adaadb095a41c3bb4bc98b.

--- include/linux/charge_level.h
+++ include/linux/charge_level.h
@@ -1,7 +1,5 @@
 /*
  * Author: andip71, 05.11.2014
- * 
- * Modified by Simao Gomes Viana
  *
  * Version 1.1
  * 
@@ -23,10 +21,10 @@ extern int charge_level;
 extern int charge_info_level_cur;
 extern int charge_info_level_req;
 
-#define AC_CHARGE_LEVEL_DEFAULT 1	// 0 = stock charging logic will apply
+#define AC_CHARGE_LEVEL_DEFAULT 0	// 0 = stock charging logic will apply
 #define AC_CHARGE_LEVEL_MIN 0
-#define AC_CHARGE_LEVEL_MAX 2000
+#define AC_CHARGE_LEVEL_MAX 2200
 
-#define USB_CHARGE_LEVEL_DEFAULT 1	// 0 = stock charging logic will apply
+#define USB_CHARGE_LEVEL_DEFAULT 0	// 0 = stock charging logic will apply
 #define USB_CHARGE_LEVEL_MIN 0
-#define USB_CHARGE_LEVEL_MAX 1500
+#define USB_CHARGE_LEVEL_MAX 1600
