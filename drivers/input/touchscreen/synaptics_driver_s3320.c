dbe9ce8238963261b818f5119d5418e681c3721f
Simao Gomes Viana <xdevs23@outlook.com>
2016-12-02 21:45:30 +0100

    synaptics_driver_s3320: fixup derp: remove finger up logspam
    Change-Id: I3c9981ce34d7dc23f73d7d492f84455b5a77c551
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1316,8 +1316,6 @@ void int_touch(void)
 	if (finger_num == 0)
 	{
 		input_report_key(ts->input_dev,BTN_TOUCH, 0);
-        if (3 == (++prlog_count % 6))
-            TPD_ERR("all finger up\n");
 		input_report_key(ts->input_dev, BTN_TOOL_FINGER, 0);
 #ifndef TYPE_B_PROTOCOL
 		input_mt_sync(ts->input_dev);
