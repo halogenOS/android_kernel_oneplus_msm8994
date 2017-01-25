906af90c555dd3d40c60a30a63f2a53b09c1daac
Tom Marshall <tdm.code@gmail.com>
2017-01-25 18:01:03 +0100

    kernel: Only expose su when daemon is running
    It has been claimed that the PG implementation of 'su' has security
    vulnerabilities even when disabled.  Unfortunately, the people that
    find these vulnerabilities often like to keep them private so they
    can profit from exploits while leaving users exposed to malicious
    hackers.
    
    In order to reduce the attack surface for vulnerabilites, it is
    therefore necessary to make 'su' completely inaccessible when it
    is not in use (except by the root and system users).
    
    Change-Id: I79716c72f74d0b7af34ec3a8054896c6559a181e
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- include/linux/uidgid.h
+++ include/linux/uidgid.h
@@ -66,6 +66,7 @@ static inline gid_t __kgid_val(kgid_t gid)
 #ifdef VENDOR_EDIT
 //huruihuan add for cgroup control
 #define GLOBAL_SYSTEM_UID KUIDT_INIT(1000)
+#define GLOBAL_SYSTEM_GID KGIDT_INIT(1000)
 #endif
 #define INVALID_UID KUIDT_INIT(-1)
 #define INVALID_GID KGIDT_INIT(-1)
