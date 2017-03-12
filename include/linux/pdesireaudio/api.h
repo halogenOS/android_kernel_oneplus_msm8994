888c8b3737657674fc9b46c5f9c70127d6b46d11
PDesire <tristan.marsell@t-online.de>
2017-03-12 09:49:26 +0100

    sound: Introduce PDesireAudio 10.0
    Codename: Yandere Audio
    
    Changelog:
    	-API added to get access to PDesireAudio everywhere
    	-Added documentation for PDesireAudio and the API
    	-Tweaked PDesireAudio and removed useless code (driver should be faster now)
    	-Fixed wrong coded samplerates and bitrates on DAI controller
    	-Set default amperage higher on SPKDRV
    	-Fixed Copyright issues (Now Team DevElite as Team Project Desire was the old name)
    
    Change-Id: I181c09ba74f61a649e96738fa2e481683d304e23
    Signed-off-by: Tristan Marsell <tristan.marsell@t-online.de>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- include/linux/pdesireaudio/api.h
+++ include/linux/pdesireaudio/api.h
@@ -1,6 +1,6 @@
-/* Copyright (C) 2016-2017 Tristan Marsell (tristan.marsell@t-online.de). All rights reserved.
- * Copyright (C) 2016-2017 Team DevElite. All rights reserved.
- *
+/* Copyright (C) 2017 Tristan Marsell (tristan.marsell@t-online.de). All rights reserved.
+ * Copyright (C) 2017 Team DevElite. All rights reserved.
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
  * only version 2 as published by the Free Software Foundation.
@@ -10,13 +10,19 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+ 
+#include "../../../sound/soc/codecs/pdesireaudio.h"
 
-#include "wcd9xxx-resmgr.h"
+extern void enable_pdesireaudio(void) {
+	pdesireaudio_start();
+	pdesireaudio_init();
+}
 
-extern void pdesireaudio_start(void);
+extern void disable_pdesireaudio(void) {
+	pdesireaudio_remove();
+	pdesireaudio_init();
+}
 
-extern void pdesireaudio_remove(void);
-
-extern void pdesireaudio_init(void);
-
-extern void pdesireaudio_advanced_mode_enable(struct snd_soc_codec *codec);
+extern void reinit_pdesireaudio(void) {
+	pdesireaudio_init();
+}
